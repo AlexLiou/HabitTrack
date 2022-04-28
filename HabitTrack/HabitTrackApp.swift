584f5ff1546f7274dfded0ba0ff09bcfeab1c4da
alexliou <alexliou0@gmail.com>
2022-04-28 14:23:56 -0400

    Initial Commit

---
+++ HabitTrack/HabitTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackApp.swift
+//  HabitTrack
+//
+//  Created by Alex Liou on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
